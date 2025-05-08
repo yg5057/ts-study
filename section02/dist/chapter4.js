12db7fa26f09bf06507c1c12308b88aae202a5ff
조연경 <sig.yg5057@gmail.com>
2025-05-08 16:04:26 +0900

    Docs: ch4.타입별칭과 인덱스 시그니처

---
+++ section02/dist/chapter4.js
@@ -0,0 +1,20 @@
+// 이렇게 만든 타입 별칭은 다음과 같이 변수의 타입을 정의할 때 타입 주석과 함께 이용할 수 있음
+let user = {
+    id: 1,
+    name: "이정환",
+    nickname: "winterlood",
+    birth: "1997.01.07",
+    bio: "안녕하세요",
+    location: "부천시",
+};
+let user2 = {
+    id: 2,
+    name: "홍길동",
+    nickname: "winterlood",
+    birth: "1997.01.07",
+    bio: "안녕하세요",
+    location: "부천시",
+};
+function test() {
+}
+export {};
---
+++ section02/src/chapter4.ts
@@ -0,0 +1,95 @@
+// 1. 타입 별칭
+//  타입 별칭을 사용하면 변수 선언하듯 타입을 별도로 정의 할 수 있음
+//  "type 타입_이름 = 타입" 형태로 타입을 정의
+type User = {
+  id: number;
+  name: string;
+  nickname: string;
+  birth: string;
+  bio: string;
+  location: string;
+};
+// 이렇게 만든 타입 별칭은 다음과 같이 변수의 타입을 정의할 때 타입 주석과 함께 이용할 수 있음
+let user: User = {
+  id: 1,
+  name: "이정환",
+  nickname: "winterlood",
+  birth: "1997.01.07",
+  bio: "안녕하세요",
+  location: "부천시",
+};
+
+let user2: User = {
+  id: 2,
+  name: "홍길동",
+  nickname: "winterlood",
+  birth: "1997.01.07",
+  bio: "안녕하세요",
+  location: "부천시",
+};
+
+// 동일한 스코프에 동일한 이름의 타입 별칭을 선언하는 것은 불가능
+// type User2 = {
+//   id: number;
+//   name: string;
+//   nickname: string;
+//   birth: string;
+//   bio: string;
+//   location: string;
+// };
+
+// type User2 = {}
+
+// 그러나 스코프가 다르다면 다음과 같이 중복된 이름으로 여러개의 별칭을 선언해도 상관 없다.
+type User2 = {
+  id: number;
+  name: string;
+  nickname: string;
+  birth: string;
+  bio: string;
+  location: string;
+};
+
+function test() {
+  type User2 = string;
+}
+
+// 2. 인덱스 시그니처
+// type CountryCodes = {
+//   Korea: string;
+//   UnitedState: string;
+//   UnitedKingdom: string;
+//    (... 약 100개의 국가)
+//   Brazil: string;
+// };
+
+// let countryCodes: CountryCodes = {
+//   Korea: "ko",
+//   UnitedState: "us",
+//   UnitedKingdom: "uk",
+//    (... 약 100개의 국가)
+//   Brazil: "bz",
+// };
+
+// ?: 이런식으로 타입별칭에도 모든 프로퍼티의 키를 넣어줘야 하므로 다음과 같이 작성한다.
+type CountryCodes = {
+  [key: string]: string;
+};
+
+let countryCodes: CountryCodes = {
+  Korea: "ko",
+  UnitedState: "us",
+  UnitedKingdom: "uk",
+  // (... 약 100개의 국가)
+  Brazil: "bz",
+};
+
+type CountryNumberCodes = {
+  [key: string]: number;
+};
+
+let countryNumberCodes: CountryNumberCodes = {
+  Korea: 410,
+  Us: 840,
+  Uk: 826,
+};
